8ecfb79eb2589d5af1286c618eb7ba384b7764ec
thatn3wguy <SteveMieskoski@gmail.com>
2017-08-18 00:52:10 -0400

    no route for user sign-up post request. oops part 3

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -20,26 +20,26 @@ module.exports = function(app, passport) {
 	}*/
 
 	// comment below and uncomment above to bypass login
-    app.post('/signup', passport.authenticate('local-signup', {
+    app.post(pageIndex.userSignUp.route, passport.authenticate('local-signup', {
         successRedirect: pageIndex.userDashboard.route,
         failureRedirect: pageIndex.userSignUp.route
     	}
 	));
 
-	app.post('/signin', passport.authenticate('local-signin', {
+	app.post(pageIndex.userSignin.route, passport.authenticate('local-signin', {
 			successRedirect: pageIndex.userDashboard.route,
 			failureRedirect: pageIndex.userSignin.route
 		}
 	));
 
 
-	app.post('/signupBuisness', passport.authenticate('local-signup', {
+	app.post(pageIndex.bizSignup.route, passport.authenticate('local-signup', {
 			successRedirect: pageIndex.bizDashboard.route,
 			failureRedirect: pageIndex.bizSignup.route
 		}
 	));
 
-	app.post('/signinBuisness', passport.authenticate('local-signin', {
+	app.post(pageIndex.bizSignin.route, passport.authenticate('local-signin', {
 			successRedirect: pageIndex.bizDashboard.route,
 			failureRedirect: pageIndex.bizSignin.route
 		}
