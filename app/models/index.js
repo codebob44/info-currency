96b4f33bc256715c2c429abd2076651f408abe4c
thatn3wguy <SteveMieskoski@gmail.com>
2017-08-18 00:32:35 -0400

    prepare for heroku deloyment

--- app/models/index.js
+++ app/models/index.js
@@ -5,7 +5,13 @@ var path = require("path");
 var Sequelize = require("sequelize");
 var env = process.env.NODE_ENV || "development";
 var config = require(path.join(__dirname, '..', 'config', 'config.json'))[env];
-var sequelize = new Sequelize(config.database, process.env.SQL_USERNAME, process.env.SQL_PASSWORD, config);
+var sequelize;
+if(process.env.JAWS_DB){
+	sequelize = new Sequelize(process.env.JAWS_DB);
+} else {
+	sequelize = new Sequelize(config.database, process.env.SQL_USERNAME, process.env.SQL_PASSWORD, config);
+}
+
 var db = {};
  
  
